f9aa7e76d18d60d6b5fe6e41754d7e067b6a5046
hxim <hxim.jo@gmail.com>
2017-07-16 10:56:36 +0200

    paq8px_v77
    changes my mpais (modified BMP parsing and record model)

--- paq8px.cpp
+++ paq8px.cpp
@@ -1989,12 +1989,25 @@ void wordModel(Mixer& m, Filetype filetype) {
 // Model 2-D data with fixed record length.  Also order 1-2 models
 // that include the distance to the last match.
 
+unsigned ilog2(unsigned x) {
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >>16);
+  return x - (x >> 1);
+}
+
+#define SPACE 0x20
+
 void recordModel(Mixer& m) {
   static int cpos1[256] , cpos2[256], cpos3[256], cpos4[256];
   static int wpos1[0x10000]; // buf(1..2) -> last position
-  static int rlen=2, rlen1=3, rlen2=4, rlen3=5;  // run length and 2 candidates
-  static int rcount1=0, rcount2=0, rcount3=0;  // candidate counts
-  static ContextMap cm(32768, 3), cn(32768/2, 3), co(32768*2, 3), cp(MEM, 3);
+  static int rlen[3] = {2,3,4}; // run length and 2 candidates
+  static int rcount[2] = {0,0}; // candidate counts
+  static U8 padding = 0; // detected padding byte
+  static int prevTransition = 0; // position of the last padding transition
+  static ContextMap cm(32768, 3), cn(32768/2, 3), co(32768*2, 3), cp(MEM, 4);
 
   // Find record length
   if (!bpos) {
@@ -2003,33 +2016,89 @@ void recordModel(Mixer& m) {
     if (r>1 && r==cpos1[c]-cpos2[c]
         && r==cpos2[c]-cpos3[c] && r==cpos3[c]-cpos4[c]
         && (r>10 || ((c==buf(r*5+1)) && c==buf(r*6+1)))) {
-      if (r==rlen1) ++rcount1;
-      else if (r==rlen2) ++rcount2;
-      else if (rcount1>rcount2) rlen2=r, rcount2=1;
-      else rlen1=r, rcount1=1;
+      if (r==rlen[1]) ++rcount[0];
+      else if (r==rlen[2]) ++rcount[1];
+      else if (rcount[0]>rcount[1]) rlen[2]=r, rcount[1]=1;
+      else rlen[1]=r, rcount[0]=1;
     }
-    if (rcount1>12 && rlen!=rlen1 && rlen1*2!=rlen1) rlen1=rlen=rlen1, rcount1=rcount2=rcount3=0;
-    if (rcount2>18 && rlen!=rlen2 && rlen1*2!=rlen2) rlen1=rlen,rlen=rlen2, rcount1=rcount2=rcount3=0;
-    if (rcount3>24 && rlen!=rlen3 && rlen1*2!=rlen3) rlen1=rlen,rlen=rlen1, rcount1=rcount2=rcount3=0;
+
+    // check candidate lengths
+    for (int i=0; i < 2; i++) {
+      if (rcount[i] > max(0,15-(int)ilog2(rlen[i+1]))){
+        if (rlen[0] != rlen[i+1]){
+          if ( (rlen[i+1] > rlen[0]) && (rlen[i+1] % rlen[0] == 0) ){
+            // maybe we found a multiple of the real record size..?
+            // in that case, it is probably an immediate multiple (2x).
+            // that is probably more likely the bigger the length, so
+            // check for small lengths too
+            if ((rlen[0] > 32) && (rlen[i+1] == rlen[0]*2)){
+              rcount[0]>>=1;
+              rcount[1]>>=1;
+              continue;
+            }
+          }
+          rlen[0] = rlen[i+1];
+          rcount[i] = 0;
+        }
+        else
+          // we found the same length again, that's positive reinforcement that
+          // this really is the correct record size, so give it a little boost
+          rcount[i]>>=2;
+
+        // if the other candidate record length is orders of
+        // magnitude larger, it will probably never have enough time
+        // to increase its counter before it's reset again. and if
+        // this length is not a multiple of the other, than it might
+        // really be worthwhile to investigate it, so we won't set its
+        // counter to 0
+        if (rlen[i+1]<<4 > rlen[1+i^1])
+          rcount[i^1] = 0;
+      }
+    }
 
     // Set 2 dimensional contexts
-    assert(rlen>0);
+    assert(rlen[0]>0);
     cm.set(c<<8| (min(255, pos-cpos1[c])/4));
     cm.set(w<<9| llog(pos-wpos1[w])>>2);
+    cm.set(rlen[0]|buf(rlen[0])<<10|buf(rlen[0]*2)<<18);
 
-    cm.set(rlen|buf(rlen)<<10|buf(rlen*2)<<18);
-    cn.set(w|rlen<<8);
-    cn.set(d|rlen<<16);
-    cn.set(c|rlen<<8);
+    cn.set(w|rlen[0]<<8);
+    cn.set(d|rlen[0]<<16);
+    cn.set(c|rlen[0]<<8);
 
     co.set(buf(1)<<8|min(255, pos-cpos1[buf(1)]));
     co.set(buf(1)<<17|buf(2)<<9|llog(pos-wpos1[w])>>2);
-    int col=pos%rlen;
-    co.set(buf(1)<<8|buf(rlen));
+    co.set(buf(1)<<8|buf(rlen[0]));
 
-    cp.set(rlen|buf(rlen)<<10|col<<18);
-    cp.set(rlen|buf(1)<<10|col<<18);
-    cp.set(col|rlen<<12);
+    int col=pos%rlen[0];
+    cp.set(rlen[0]|buf(rlen[0])<<10|col<<18);
+    cp.set(rlen[0]|buf(1)<<10|col<<18);
+    cp.set(col|rlen[0]<<12);
+
+    /*
+    Consider record structures that include fixed-length strings.
+    These usually contain the text followed by either spaces or 0's,
+    depending on whether they're to be trimmed or they're null-terminated.
+    That means we can guess the length of the string field by looking
+    for small repetitions of one of these padding bytes followed by a
+    different byte. By storing the last position where this transition
+    ocurred, and what was the padding byte, we are able to model these
+    runs of padding bytes.
+    Special care is taken to skip record structures of less than 9 bytes,
+    since those may be little-endian 64 bit integers. If they contain
+    relatively low values (<2^40), we may consistently get runs of 3 or
+    even more 0's at the end of each record, and so we could assume that
+    to be the general case. But with integers, we can't be reasonably sure
+    that a number won't have 3 or more 0's just before a final non-zero MSB.
+    And with such simple structures, there's probably no need to be fancy
+    anyway
+    */
+
+    if ((((U16)(c4>>8) == ((SPACE<<8)+SPACE)) && (c0 != SPACE)) || (!(c4>>8) && c0 && ((padding != SPACE) || (pos-prevTransition > rlen[0])))){
+      prevTransition = pos;
+      padding = (U8)d;
+    }
+    cp.set( (rlen[0]>8)*hash( min(min(0xFF,rlen[0]),pos-prevTransition), min(0x3FF,col), w&0xF0F0 + (w==((padding<<8)+padding)) ) );
 
     // update last context positions
     cpos4[c]=cpos3[c];
@@ -2500,9 +2569,9 @@ int jpegModel(Mixer& m) {
     // FF 00 is interpreted as FF (to distinguish from RSTx, DNL, EOI).
 
     // Detect JPEG (SOI, APPx)
-    if (!jpeg && buf(4)==FF && buf(3)==SOI && buf(2)==FF && buf(1)>>4==0xe) {
+    if (!jpeg && buf(4)==FF && buf(3)==SOI && buf(2)==FF && (buf(1)==0xC0 || buf(1)==0xC4 || (buf(1)>=0xDB && buf(1)<=0xFE)) ){
       jpeg=1;
-      sos=sof=htsize=data=mcusize=linesize=0, app=2;
+      sos=sof=htsize=data=mcusize=linesize=0, app=(buf(1)>>4==0xE)*2;
       huffcode=huffbits=huffsize=mcupos=cpos=0, rs=-1;
       memset(&huf[0], 0, huf.size()*sizeof(HUF));
       memset(&pred[0], 0, pred.size()*sizeof(int));
@@ -3328,7 +3397,7 @@ void nestModel(Mixer& m)
 int contextModel2() {
   static ContextMap cm(MEM*32, 9);
   static RunContextMap rcm7(MEM), rcm9(MEM), rcm10(MEM);
-  static Mixer m(881, 3095, 7);
+  static Mixer m(881+8, 3095, 7);
   static U32 cxt[16];  // order 0-11 contexts
   static Filetype ft2,filetype=DEFAULT;
   static int size=0;  // bytes remaining in block
@@ -3778,7 +3847,7 @@ Filetype detect(FILE* in, int n, Filetype type, int &info) {
   int wavi=0,wavsize=0,wavch=0,wavbps=0,wavm=0;  // For WAVE detection
   int aiff=0,aiffm=0,aiffs=0;  // For AIFF detection
   int s3mi=0,s3mno=0,s3mni=0;  // For S3M detection
-  int bmp=0,imgbpp=0,bmpx=0,bmpy=0,bmpof=0;  // For BMP detection
+  int bmp=0,imgbpp=0,bmpx=0,bmpy=0,bmpof=0,hdrless=0;  // For BMP detection
   int rgbi=0,rgbx=0,rgby=0;  // For RGB detection
   int tga=0,tgax=0,tgay=0,tgaz=0,tgat=0;  // For TGA detection
   int pgm=0,pgmcomment=0,pgmw=0,pgmh=0,pgm_ptr=0,pgmc=0,pgmn=0;  // For PBM, PGM, PPM detection
@@ -3902,7 +3971,7 @@ Filetype detect(FILE* in, int n, Filetype type, int &info) {
     // Detect end by any code other than RST0-RST7 (FF D9-D7) or
     // a byte stuff (FF 00).
 
-    if (!soi && i>=3 && (buf0&0xfffffff0)==0xffd8ffe0) soi=i, app=i+2, sos=sof=0;
+    if (!soi && i>=3 && (buf0&0xffffff00)==0xffd8ff00 && ((U8)buf0==0xC0 || (U8)buf0==0xC4 || ((U8)buf0>=0xDB && (U8)buf0<=0xFE) )) soi=i, app=i+2, sos=sof=0;
     if (soi) {
       if (app==i && (buf0>>24)==0xff &&
          ((buf0>>16)&0xff)>0xc0 && ((buf0>>16)&0xff)<0xff) app=i+(buf0&0xffff)+2;
@@ -3999,16 +4068,33 @@ Filetype detect(FILE* in, int n, Filetype type, int &info) {
     }
 
     // Detect .bmp image
-    if ((buf0&0xffff)==16973) imgbpp=bmpx=bmpy=bmpof=0,bmp=i;  //possible 'BM'
+    if ( !bmp && (((buf0&0xffff)==16973) || (!(buf0&0xFFFFFF) && ((buf0>>24)==0x28))) ) //possible 'BM' or headerless DIB
+      imgbpp=bmpx=bmpy=0,hdrless=!(U8)buf0,bmpof=hdrless*68,bmp=i-hdrless*16;
     if (bmp) {
       const int p=i-bmp;
       if (p==12) bmpof=bswap(buf0);
-      else if (p==16 && buf0!=0x28000000) bmp=0; //windows bmp?
+      else if (p==16 && buf0!=0x28000000) bmp=0; //BITMAPINFOHEADER (0x28)
       else if (p==20) bmpx=bswap(buf0),bmp=((bmpx==0||bmpx>0x30000)?0:bmp); //width
       else if (p==24) bmpy=abs((int)bswap(buf0)),bmp=((bmpy==0||bmpy>0x10000)?0:bmp); //height
       else if (p==27) imgbpp=c,bmp=((imgbpp!=1 && imgbpp!=8 && imgbpp!=24)?0:bmp);
-      else if (p==31) {
-        if (imgbpp!=0 && buf0==0) {
+      else if ((p==31) && buf0) bmp=0;
+      // check number of colors in palette (4 bytes), must be 0 (default) or <= 1<<bpp.
+      // also check if image is too small, since it might not be worth it to use the image models
+      else if (p==48){
+        if ( (!buf0 || ((bswap(buf0)<=(U32)(1<<imgbpp)) && (imgbpp<=8))) && (((bmpx*bmpy*imgbpp)>>3)>256) ) {
+          // possible icon/cursor?
+          if (hdrless && (bmpx*2==bmpy) && (
+            (bmpx==8)   || (bmpx==10) || (bmpx==14) || (bmpx==16) || (bmpx==20) ||
+            (bmpx==22)  || (bmpx==24) || (bmpx==32) || (bmpx==40) || (bmpx==48) ||
+            (bmpx==60)  || (bmpx==64) || (bmpx==72) || (bmpx==80) || (bmpx==96) ||
+            (bmpx==128) || (bmpx==256)
+          ))
+            bmpy=bmpx;
+
+          // if DIB and not 24bpp, we must calculate the data offset based on BPP or num. of entries in color palette
+          if (hdrless && (imgbpp!=24))
+            bmpof+=(buf0)?bswap(buf0)*4:4<<imgbpp;
+
           if (imgbpp==1) IMG_DET(IMAGE1,bmp-1,bmpof,(((bmpx-1)>>5)+1)*4,bmpy);
           else if (imgbpp==8) IMG_DET(IMAGE8,bmp-1,bmpof,(bmpx+3)&-4,bmpy);
           else if (imgbpp==24) IMG_DET(IMAGE24,bmp-1,bmpof,((bmpx*3)+3)&-4,bmpy);
