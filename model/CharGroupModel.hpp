49306a68f902f93e920621a3064cd64ea446da54
Zoltán Gotthardt <gotty@freemail.hu>
2020-11-08 00:04:05 +0100

    Cosmetic changes in CharGroupModel

--- model/CharGroupModel.hpp
+++ model/CharGroupModel.hpp
@@ -22,9 +22,9 @@ private:
     static constexpr int nCM = 7;
     const Shared * const shared;
     ContextMap2 cm;
-    uint32_t gAscii1 = 0; /**< group identifiers of the last 12 (4+4+4) characters; the most recent is @ref gAscii1 **/
-    uint32_t gAscii2 = 0;
-    uint32_t gAscii3 = 0;
+    uint32_t g1 = 0; /**< collapsed group identifiers of the last 12 (4+4+4) characters; the most recent is @ref gAscii1 **/
+    uint32_t g2 = 0;
+    uint32_t g3 = 0;
 public:
     static constexpr int MIXERINPUTS =
             nCM * (ContextMap2::MIXERINPUTS + ContextMap2::MIXERINPUTS_RUN_STATS + ContextMap2::MIXERINPUTS_BYTE_HISTORY); // 35
