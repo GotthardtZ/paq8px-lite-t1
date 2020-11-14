ddb44631b36a75b03d85dee0c82bed76cbf6c1f4
Zoltán Gotthardt <gotty@freemail.hu>
2020-11-14 23:29:21 +0100

    New model: SparseBitModel (sparse bit contexts extracted from SparseModel)

--- model/ContextModel.cpp
+++ model/ContextModel.cpp
@@ -7,25 +7,25 @@ ContextModel::ContextModel(Shared* const sh, Models &models) : shared(sh), model
     sh,
     1 +  //bias
     MatchModel::MIXERINPUTS + NormalModel::MIXERINPUTS + SparseMatchModel::MIXERINPUTS +
-    SparseModel::MIXERINPUTS + RecordModel::MIXERINPUTS + CharGroupModel::MIXERINPUTS +
+    SparseModel::MIXERINPUTS + SparseBitModel::MIXERINPUTS + RecordModel::MIXERINPUTS + CharGroupModel::MIXERINPUTS +
     TextModel::MIXERINPUTS + WordModel::MIXERINPUTS + IndirectModel::MIXERINPUTS +
     DmcForest::MIXERINPUTS + NestModel::MIXERINPUTS + XMLModel::MIXERINPUTS +
     LinearPredictionModel::MIXERINPUTS + ExeModel::MIXERINPUTS + LstmModel<>::MIXERINPUTS +
     DECAlphaModel::MIXERINPUTS
     ,
     MatchModel::MIXERCONTEXTS + NormalModel::MIXERCONTEXTS + SparseMatchModel::MIXERCONTEXTS +
-    SparseModel::MIXERCONTEXTS + RecordModel::MIXERCONTEXTS + CharGroupModel::MIXERCONTEXTS +
+    SparseModel::MIXERCONTEXTS + SparseBitModel::MIXERCONTEXTS + RecordModel::MIXERCONTEXTS + CharGroupModel::MIXERCONTEXTS +
     TextModel::MIXERCONTEXTS + WordModel::MIXERCONTEXTS + IndirectModel::MIXERCONTEXTS +
     DmcForest::MIXERCONTEXTS + NestModel::MIXERCONTEXTS + XMLModel::MIXERCONTEXTS +
     LinearPredictionModel::MIXERCONTEXTS + ExeModel::MIXERCONTEXTS + LstmModel<>::MIXERCONTEXTS +
-    DECAlphaModel::MIXERCONTEXTS
+    DECAlphaModel::MIXERCONTEXTS 
     ,
     MatchModel::MIXERCONTEXTSETS + NormalModel::MIXERCONTEXTSETS + SparseMatchModel::MIXERCONTEXTSETS +
-    SparseModel::MIXERCONTEXTSETS + RecordModel::MIXERCONTEXTSETS + CharGroupModel::MIXERCONTEXTSETS +
+    SparseModel::MIXERCONTEXTSETS + SparseBitModel::MIXERCONTEXTSETS + RecordModel::MIXERCONTEXTSETS + CharGroupModel::MIXERCONTEXTSETS +
     TextModel::MIXERCONTEXTSETS + WordModel::MIXERCONTEXTSETS + IndirectModel::MIXERCONTEXTSETS +
     DmcForest::MIXERCONTEXTSETS + NestModel::MIXERCONTEXTSETS + XMLModel::MIXERCONTEXTSETS +
     LinearPredictionModel::MIXERCONTEXTSETS + ExeModel::MIXERCONTEXTSETS + +LstmModel<>::MIXERCONTEXTSETS +
-    DECAlphaModel::MIXERCONTEXTSETS
+    DECAlphaModel::MIXERCONTEXTSETS 
   );
 }
 
@@ -193,6 +193,8 @@ auto ContextModel::p() -> int {
   if( blockType != IMAGE1 ) {
     SparseMatchModel &sparseMatchModel = models.sparseMatchModel();
     sparseMatchModel.mix(*m);
+    SparseBitModel& sparseBitModel = models.sparseBitModel();
+    sparseBitModel.mix(*m);
     SparseModel &sparseModel = models.sparseModel();
     sparseModel.mix(*m);
     RecordModel &recordModel = models.recordModel();
