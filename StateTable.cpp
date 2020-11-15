3f5293b6dd65870ad4f830690b17ea5e51b45f10
Zoltán Gotthardt <gotty@freemail.hu>
2020-11-15 03:44:26 +0100

    Simplified probabilistic increment in StateTable

--- StateTable.cpp
+++ StateTable.cpp
@@ -8,9 +8,8 @@ auto StateTable::next(uint8_t const oldState, const int y, Random &rnd) -> uint8
   uint8_t newState = stateTable[oldState][y];
   if( newState >= 205 ) { // for all groups of four states higher than idx 205
     if( oldState < 249 ) { // still climbing
-      const int shift = (460 - newState) >> 3U;
       //for each group the probability to advance to a higher state is less and less as we climb higher and higher
-      if( rnd(32-shift) != 0 ) {
+      if( rnd(((newState-205)>>3)+1) != 0 ) {
         newState -= 4;
       }
     }
