ae1e9bf7a8c180a257f188e5eb93150c6f80e2df
Zoltán Gotthardt <gotty@freemail.hu>
2020-12-31 23:02:21 +0100

    paq8px_v200

--- paq8px.cpp
+++ paq8px.cpp
@@ -8,7 +8,7 @@
 //////////////////////// Versioning ////////////////////////////////////////
 
 #define PROGNAME     "paq8px"
-#define PROGVERSION  "199"  //update version here before publishing your changes
+#define PROGVERSION  "200"  //update version here before publishing your changes
 #define PROGYEAR     "2020"
 
 
