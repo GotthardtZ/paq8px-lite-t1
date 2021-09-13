71a6d73749db01cb03fba28f2679f22870ac08b7
Zoltán Gotthardt <gotty@freemail.hu>
2021-09-13 22:50:09 +0200

    Switched from ContextMap to ContextMap2 in NestModel (without Run Stats) in IndirectModel and XMLModel (with Run Stats).

--- model/IndirectModel.cpp
+++ model/IndirectModel.cpp
@@ -1,6 +1,6 @@
 #include "IndirectModel.hpp"
 
-IndirectModel::IndirectModel(const Shared* const sh, const uint64_t size) : shared(sh), cm(sh, size, nCM) {}
+IndirectModel::IndirectModel(const Shared* const sh, const uint64_t size) : shared(sh), cm(sh, size, nCM, 64) {}
 
 void IndirectModel::mix(Mixer &m) {
   INJECT_SHARED_bpos
@@ -34,24 +34,26 @@ void IndirectModel::mix(Mixer &m) {
     uint16_t& r4 = t4[(buf(2) >> 4) | (buf(3) >> 4) << 4 | (buf(4) >> 4) << 8 | (buf(5) >> 4) << 12];
     r4 = r4 << 8 | c1;
 
+    const uint8_t R_ = CM_USE_RUN_STATS; 
     uint64_t i = 0;
 
-    cm.set(hash(++i, h1));              // last 1 byte + 3 history bytes {in a 1-byte context}
-    cm.set(hash(++i, h1 & 0xffffff00)); // 3 history bytes {in a 1-byte context}
-    cm.set(hash(++i, t1[c1]));          // 4 history bytes {in a 1-byte context}
-    cm.set(hash(++i, h2));              // last 2 bytes + 2 history bytes {in a 2-byte context}
-    cm.set(hash(++i, h3));              // last 3 top 5 bits + 2 history bytes {in a "last 3 top 5 bits" context}
-    cm.set(hash(++i, h4));              // last 4 top 4 bits + 2 history bytes {in a "last 4 top 4 bits" context}
     
-    cm.set(hash(++i, h1 & 0x0000ff00)); // 1 history byte {in a 1-byte context}
-    cm.set(hash(++i, h2 & 0x00ff0000)); // 1 history byte {in a 2-byte context}
-    cm.set(hash(++i, h3 & 0x00ff0000)); // last 3 top 5 bits + 2 history bytes {in a "last 3 top 5 bits" context}
-    cm.set(hash(++i, h4 & 0x00ff0000)); // last 4 top 4 bits + 2 history bytes {in a "last 4 top 4 bits" context}
+    cm.set(R_,hash(++i, h1));              // last 1 byte + 3 history bytes {in a 1-byte context}
+    cm.set(R_, hash(++i, h1 & 0xffffff00)); // 3 history bytes {in a 1-byte context}
+    cm.set(R_, hash(++i, t1[c1]));          // 4 history bytes {in a 1-byte context}
+    cm.set(R_, hash(++i, h2));              // last 2 bytes + 2 history bytes {in a 2-byte context}
+    cm.set(R_, hash(++i, h3));              // last 3 top 5 bits + 2 history bytes {in a "last 3 top 5 bits" context}
+    cm.set(R_, hash(++i, h4));              // last 4 top 4 bits + 2 history bytes {in a "last 4 top 4 bits" context}
+    
+    cm.set(R_, hash(++i, h1 & 0x0000ff00)); // 1 history byte {in a 1-byte context}
+    cm.set(R_, hash(++i, h2 & 0x00ff0000)); // 1 history byte {in a 2-byte context}
+    cm.set(R_, hash(++i, h3 & 0x00ff0000)); // last 3 top 5 bits + 2 history bytes {in a "last 3 top 5 bits" context}
+    cm.set(R_, hash(++i, h4 & 0x00ff0000)); // last 4 top 4 bits + 2 history bytes {in a "last 4 top 4 bits" context}
    
-    cm.set(hash(++i, h1 & 0x0000ffff)); // last byte + 1 history byte {in a 1-byte context}
-    cm.set(hash(++i, h2 & 0x00ffffff)); // last 2 bytes + 1 history byte {in a 2-byte context}
-    cm.set(hash(++i, h3 & 0x00ffffff)); // last 3 top 5 bits + 1 history byte {in a "last 3 top 5 bits" context}
-    cm.set(hash(++i, h4 & 0x00ffffff)); // last 4 top 4 bits + 1 history byte {in a "last 4 top 4 bits" context}
+    cm.set(R_, hash(++i, h1 & 0x0000ffff)); // last byte + 1 history byte {in a 1-byte context}
+    cm.set(R_, hash(++i, h2 & 0x00ffffff)); // last 2 bytes + 1 history byte {in a 2-byte context}
+    cm.set(R_, hash(++i, h3 & 0x00ffffff)); // last 3 top 5 bits + 1 history byte {in a "last 3 top 5 bits" context}
+    cm.set(R_, hash(++i, h4 & 0x00ffffff)); // last 4 top 4 bits + 1 history byte {in a "last 4 top 4 bits" context}
 
     // context with 2 characters converted to lowercase (context table: t5, byte history: h5)
 
@@ -62,10 +64,10 @@ void IndirectModel::mix(Mixer &m) {
     uint32_t& r5 = t5[(chars4 >> 8) & 0xffff];
     r5 = r5 << 8 | c;
 
-    cm.set(hash(++i, (h5 & 0xff) | c << 8)); //last char + 1 history char
-    cm.set(hash(++i, h5 & 0xffff)); //2 history chars
-    cm.set(hash(++i, h5 & 0xffffff)); //3 history chars
-    cm.set(hash(++i, h5)); //4 history chars
+    cm.set(R_, hash(++i, (h5 & 0xff) | c << 8)); //last char + 1 history char
+    cm.set(R_, hash(++i, h5 & 0xffff)); //2 history chars
+    cm.set(R_, hash(++i, h5 & 0xffffff)); //3 history chars
+    cm.set(R_, hash(++i, h5)); //4 history chars
 
     //
     // large contexts (with hashtable)
@@ -76,23 +78,23 @@ void IndirectModel::mix(Mixer &m) {
     iCtxLarge.set(hash(i, c4 >> 8), c1);
     context = c4 & 0xffffff; //24 bits
     h6 = iCtxLarge.get(hash(i, context));
-    cm.set(hash(++i, h6 & 0xff, context & 0xff));
-    cm.set(hash(++i, h6 & 0xff, context & 0xffff));
-    cm.set(hash(++i, h6, context & 0xff)); //without context is also OK (not for news and obj2 however)
+    cm.set(R_, hash(++i, h6 & 0xff, context & 0xff));
+    cm.set(R_, hash(++i, h6 & 0xff, context & 0xffff));
+    cm.set(R_, hash(++i, h6, context & 0xff)); //without context is also OK (not for news and obj2 however)
 
     iCtxLarge.set(hash(i, chars4 >> 8), c1);
     context = chars4 & 0xffffff; //24 bits lowercase
     h6 = iCtxLarge.get(hash(i, context));
-    cm.set(hash(++i, h6 & 0xff, context));
-    cm.set(hash(++i, h6 & 0xffff, context));
-    cm.set(hash(++i, h6));
+    cm.set(R_, hash(++i, h6 & 0xff, context));
+    cm.set(R_, hash(++i, h6 & 0xffff, context));
+    cm.set(R_, hash(++i, h6));
 
     iCtxLarge.set(hash(i, buf(5) << 24 | c4 >> 8), c1);
     context = c4; //32 bits
     h6 = iCtxLarge.get(hash(i, context));
-    cm.set(hash(++i, h6 & 0xff, context &0xffff));
-    cm.set(hash(++i, h6 & 0xffff, context & 0xff));
-    cm.set(hash(++i, h6));
+    cm.set(R_, hash(++i, h6 & 0xff, context &0xffff));
+    cm.set(R_, hash(++i, h6 & 0xffff, context & 0xff));
+    cm.set(R_, hash(++i, h6));
 
     assert(i == nCM);
   }
--- model/NestModel.hpp
+++ model/NestModel.hpp
@@ -2,17 +2,17 @@
 #define PAQ8PX_NESTMODEL_HPP
 
 #include "../Shared.hpp"
-#include "../ContextMap.hpp"
+#include "../ContextMap2.hpp"
 
 class NestModel {
 private:
     static constexpr int nCM = 12;
     const Shared * const shared;
     int ic = 0, bc = 0, pc = 0, vc = 0, qc = 0, lvc = 0, wc = 0, ac = 0, ec = 0, uc = 0, sense1 = 0, sense2 = 0, w = 0;
-    ContextMap cm;
+    ContextMap2 cm;
 
 public:
-    static constexpr int MIXERINPUTS = nCM * (ContextMap::MIXERINPUTS); // 60
+    static constexpr int MIXERINPUTS = nCM * (ContextMap2::MIXERINPUTS + ContextMap2::MIXERINPUTS_RUN_STATS); // 60
     static constexpr int MIXERCONTEXTS = 0;
     static constexpr int MIXERCONTEXTSETS = 0;
     explicit NestModel(const Shared* const sh, uint64_t size);
