05c8936dcd9111502ccd8732b7eeda1f2f08a461
Zoltán Gotthardt <gotty@freemail.hu>
2021-09-22 20:25:33 +0200

    Improved ChartModel.

--- model/ChartModel.hpp
+++ model/ChartModel.hpp
@@ -7,22 +7,21 @@
 
 class ChartModel {
 private:
-    static constexpr int nCM1 = (2 * 8 + 4) * 3;
-    static constexpr int nCM2 = 3 * 5 + 8;
-    static constexpr int nCM1_TEXT = 8 * 3;
-    static constexpr int nCM2_TEXT = 1 * 5 + 8;
+  static constexpr int nCM = (3 * 4) + 3 + (8 + 8 + 8) * 3; //87
+  static constexpr int nCM_TEXT = (3 * 4) + 2 + (8) * 3; //38
     const Shared * const shared;
-    ContextMap2 cm, cn;
-    uint32_t chart[2*8+4]{};
+    ContextMap2 cm;
+    uint32_t chart[3*8]{};
+    uint32_t charGroup{};
     uint8_t indirect1[1024+64]{};
     uint8_t indirect2[256]{};
     uint8_t indirect3[256 * 256]{};
 public:
-    static constexpr int MIXERINPUTS = (nCM1+nCM2) * (ContextMap2::MIXERINPUTS); // 332
-    static constexpr int MIXERINPUTS_TEXT = (nCM1_TEXT + nCM2_TEXT) * (ContextMap2::MIXERINPUTS); // 148
+    static constexpr int MIXERINPUTS = nCM * (ContextMap2::MIXERINPUTS); // 348
+    static constexpr int MIXERINPUTS_TEXT = nCM_TEXT * (ContextMap2::MIXERINPUTS); // 152
     static constexpr int MIXERCONTEXTS = 0;
     static constexpr int MIXERCONTEXTSETS = 0;
-    explicit ChartModel(const Shared* const sh, const uint64_t size1, const uint64_t size2);
+    explicit ChartModel(const Shared* const sh, const uint64_t size);
     void mix(Mixer &m);
 };
 
